3bacdb95639009e146697c5b29be97cbba51dbc9
harshita <hrs.dadhich@gmail.com>
2024-04-16 12:41:36 +0530

    FORMATTED

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,25 +1,29 @@
 """"
 Django command to wait for database to be available
 """
+
 from django.core.management.base import BaseCommand
 import time
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 
+
 class Command(BaseCommand):
     """Django command to wait for database"""
+
     def handle(self, *args, **options):
         self.stdout.write("waiting for database...")
-        db_up=False
+        db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
-                db_up=True
-            except(Psycopg2OpError, OperationalError):
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write("database unavailable, waiting 1 second")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available')) 
+        self.stdout.write(self.style.SUCCESS("Database available"))
+
 
 # the command inherit from the basecommand, which is the base class for all management commands in Django.
-# the handle method is the entry point for the management command. it is called when the command is executed. 
+# the handle method is the entry point for the management command. it is called when the command is executed.
