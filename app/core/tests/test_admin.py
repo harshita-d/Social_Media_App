2921bd19cb6a704bf5c4a3e172ed959f7e622eb9
harshita <hrs.dadhich@gmail.com>
2024-04-18 20:08:47 +0530

    test for admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,43 @@
+"""tests for Django admin modifications"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+# Client is a django test client that is used make http request
+
+
+class AdminSiteTests(TestCase):
+    """Test for Django Admin"""
+
+    # the "setUp" should be like this as its a default test name that will run before every test
+    def setUp(self):
+        """create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="123",
+        )
+
+        # force login allows to make an HTTP request authenticated
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com",
+            password="123",
+            name="Test USer",
+        )
+
+    def test_user_list(self):
+        """tests that users are listed on the page"""
+
+        # when admin site is deployed, the views provided by that site are accessible using Django URL reversing system
+        # reverse is use to get the url for changelist inside Django app
+        # admin:core_user_changelist is a syntax to determines which url we will pull
+        # changelist is a page of list of users
+        url = reverse("admin:core_user_changelist")
+
+        # self.client.get makes HTTP request
+        res = self.client.get(url)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
