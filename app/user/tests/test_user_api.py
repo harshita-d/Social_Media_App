0c30a8401330ea90f8e6ce8164260b63685531c3
harshita <hrs.dadhich@gmail.com>
2024-04-26 23:35:35 +0530

    user api test

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+# The APIClient class from Django Rest Framework's test module is used for testing API endpoints in Django applications.
+# It allows you to make requests to your API endpoints directly from your test code, without needing to run a server.
+
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse("user:create")
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_success(self):
+        payload = {
+            "email": "test@example.com",
+            "password": "123",
+            "name": "Test Name",
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        # checking if API response is 201
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        # checking in API if the email sending in payload is equal to the email we are getting in response
+        # this will retrieve object from DB with sending email through which we created account
+        user = get_user_model().objects.get(email=payload["email"])
+
+        # the below line checks if the password we are getting in object is same as we send in payload
+        self.assertTrue(user.check_password(payload["password"]))
+
+        # here we are checking that the saved password is not returned in response as it can cause a security issue
+        self.assertNotIn("password", res.data)
